8665add5f7abe06667c4befbaa814c8b3ea1fbf1
MounirBlk <mounir.ballouk@gmail.com>
2020-03-31 15:39:45 +0200

    partie login

--- src/config/structure.js
+++ src/config/structure.js
@@ -32,5 +32,6 @@ module.exports = {
     logger: {
         host: '',
         port: '',
-    }
+    },
+    keyToken: ''
 };
--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,8 +1,36 @@
-const User = require('../models/user'),
+const config = require('../config'),
+    jwt = require('jsonwebtoken'),
+    User = require('../models/user'),
     filter = require('../middleware/filter');
 
-exports.login = (req, res) => {
-    res.status(200).send('OK');
+exports.login = async(req, res) => {
+    const data = Object.assign({ identifiant: '', password: '' }, req.body)
+
+    const where = (!filter.email(data.identifiant)) ? { username: data.identifiant } : { email: data.identifiant }
+    const user = await User.findOne(where);
+
+    if (user === null)
+        return res.status(409).json({
+            error: true,
+            message: 'Email/Password error'
+        })
+
+    const valid = user.verifyPasswordSync(data.password);
+    if (valid) {
+        return res.status(200).json({
+            error: false,
+            token: jwt.sign({
+                id: user.get('_id'),
+                exp: Math.floor(Date.now() / 1000) + (60 * 60) * 24 * 7,
+            }, config.keyToken)
+        })
+    } else {
+        return res.status(409).json({
+            error: true,
+            message: 'Email/Password error'
+        })
+
+    }
 }
 
 exports.register = (req, res) => {
@@ -15,11 +43,14 @@ exports.register = (req, res) => {
         })
 
     new User({ email: data.email, password: data.password, username: data.username })
-        .save().then(() => {
+        .save().then((user) => {
             console.log('User register');
-            res.status(200).json({
+            return res.status(200).json({
                 error: false,
-                message: ''
+                token: jwt.sign({
+                    id: user.get('_id'),
+                    exp: Math.floor(Date.now() / 1000) + (60 * 60) * 24 * 7,
+                }, config.keyToken)
             })
         }).catch((error) => {
             return res.status(409).json({
@@ -30,5 +61,7 @@ exports.register = (req, res) => {
 }
 
 exports.forgot_password = (req, res) => {
+    var decoded = jwt.verify('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVlODM0Njg3ZWUwN2RjMzU1ZGUxNDQxZCIsImV4cCI6MTU4NjI2NjM3NSwiaWF0IjoxNTg1NjYxNTc1fQ.sSqrgr3CZo837erHwQ6VDnlh_BzY1aMGT9g5lvs4s30', config.keyToken);
+    console.log(decoded) // bar
     res.status(200).send('OK');
 }
